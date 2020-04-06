ed548393ce255eb853d5bcf1ea88e27102634857
Zeljko Bilbija <bilbijazeljko@gmail.com>
2020-04-07 01:52:43 +0200

    Dodati xml fajlovi za katihizis, pitanja i odgovori

--- app/src/main/java/com/interfacemockup/kalendar/DetailKalendar.java
+++ app/src/main/java/com/interfacemockup/kalendar/DetailKalendar.java
@@ -13,6 +13,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.interfacemockup.kalendar.pravoslavnekalkulacije.PravoslavneKonstante;
+import com.interfacemockup.kalendar.pravoslavnekalkulacije.PravoslavniKalendar;
 
 public class DetailKalendar extends AppCompatActivity {
 
@@ -56,6 +57,15 @@ public class DetailKalendar extends AppCompatActivity {
                 break;
             case 1:
                 // Februar
+                if (PravoslavniKalendar.getInstance().prestupnaGodina()){
+                    _ikona.setImageResource(_konst.drawables_februar_big[dan]);
+                    _svetac.setText(getResources().getStringArray(R.array.svetitelji_februar_big)[dan]);
+                    _zitije.setText(getResources().getStringArray(R.array.zitija_februar_big)[dan]);
+                }else {
+                    _ikona.setImageResource(_konst.drawables_februar_small[dan]);
+                    _svetac.setText(getResources().getStringArray(R.array.svetitelji_februar_small)[dan]);
+                    _zitije.setText(getResources().getStringArray(R.array.zitija_februar_small)[dan]);
+                }
                 break;
             case 2:
                 //Mart
@@ -73,34 +83,58 @@ public class DetailKalendar extends AppCompatActivity {
                 break;
             case 4:
                 //Maj
+                _ikona.setImageResource(_konst.drawables_maj[dan]);
+                _svetac.setText(getResources().getStringArray(R.array.svetitelji_majs)[dan]);
+                _zitije.setText(getResources().getStringArray(R.array.zitija_maj)[dan]);
 
                 break;
             case 5:
                 //JUN
+                _ikona.setImageResource(_konst.drawables_jun[dan]);
+                _svetac.setText(getResources().getStringArray(R.array.svetitelji_jun)[dan]);
+                _zitije.setText(getResources().getStringArray(R.array.zitija_jun)[dan]);
 
                 break;
             case 6:
                 //Jul
+                _ikona.setImageResource(_konst.drawables_jul[dan]);
+                _svetac.setText(getResources().getStringArray(R.array.svetitelji_jul)[dan]);
+                _zitije.setText(getResources().getStringArray(R.array.zitija_jul)[dan]);
 
                 break;
             case 7:
                 //Avgust
+                _ikona.setImageResource(_konst.drawables_avgust[dan]);
+                _svetac.setText(getResources().getStringArray(R.array.svetitelji_avgust)[dan]);
+                _zitije.setText(getResources().getStringArray(R.array.zitija_avgust)[dan]);
 
                 break;
             case 8:
                 //Septembar
+                _ikona.setImageResource(_konst.drawables_septembar[dan]);
+                _svetac.setText(getResources().getStringArray(R.array.svetitelji_septembar)[dan]);
+                _zitije.setText(getResources().getStringArray(R.array.zitija_septembar)[dan]);
 
                 break;
             case 9:
                 //Oktobar
+                _ikona.setImageResource(_konst.drawables_oktobar[dan]);
+                _svetac.setText(getResources().getStringArray(R.array.svetitelji_oktobar)[dan]);
+                _zitije.setText(getResources().getStringArray(R.array.zitija_oktobar)[dan]);
 
                 break;
             case 10:
                 //Nobvembar
+                _ikona.setImageResource(_konst.drawables_novembar[dan]);
+                _svetac.setText(getResources().getStringArray(R.array.svetitelji_novembar)[dan]);
+                _zitije.setText(getResources().getStringArray(R.array.zitija_novembar)[dan]);
 
                 break;
             case 11:
                 //Decembar
+                _ikona.setImageResource(_konst.drawables_decembar[dan]);
+                _svetac.setText(getResources().getStringArray(R.array.svetitelji_decembar)[dan]);
+                _zitije.setText(getResources().getStringArray(R.array.zitija_decembar)[dan]);
 
                 break;
             default:
